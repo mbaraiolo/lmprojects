688494781090739cba1cfccb2f66e6d3eace6e3d
unknown <moreno.baraiolo@gmail.com>
2020-11-08 17:55:17 +0100

    first commit

---
+++ test_01/sales.py
@@ -0,0 +1,174 @@
+import math
+
+# round up a float to nearest 0.05
+def roundup_taxes (x):
+    return math.ceil(x * 20) / 20
+
+# rappresent a shopping item in a shooping bag
+class shopping_item: 
+
+    def __init__(self):
+
+        self._name = None
+        self._price = 0
+        self._quantity = 1
+        self._tax = 0
+        self._category = None
+        self._imported = False
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def price(self): 
+        return self._price
+    
+    @property
+    def quantity(self): 
+        return self._quantity
+
+    @property
+    def tax(self): 
+        return self._tax
+
+    @property
+    def category(self): 
+        return self._category
+
+    @property
+    def imported(self): 
+        return self._imported
+
+    @name.setter 
+    def name(self, a):
+        if not isinstance(a, str):
+            raise TypeError("name must be set to an string")
+        self._name = a 
+
+    @quantity.setter 
+    def quantity(self, a):
+        if not isinstance(a, int):
+            raise TypeError("quantity must be set to an int")
+        self._quantity = a 
+
+    @price.setter 
+    def price(self, a): 
+        if a < 0: 
+            raise ValueError("price can't be below zero") 
+        elif not isinstance(a, float):  
+            raise ValueError("price must be set to an float") 
+        self._price = a
+    
+    @tax.setter 
+    def tax(self, a):
+        if a < 0: 
+            raise ValueError("tax can't be below zero") 
+        elif not isinstance(a, float) : 
+            raise ValueError("tax must be set to an float") 
+        self._tax = a 
+
+    @category.setter 
+    def category(self, a): 
+        self._category = a 
+
+    @imported.setter 
+    def imported(self, a):
+        if not isinstance(a, bool):
+            raise TypeError("imported must be set to an bool")
+        self._imported = a 
+
+# rappresent a shooping basket
+class shopping_basket ():
+
+    def __init__(self): 
+        self._items = []
+
+    @property
+    def items(self): 
+        return self._items 
+
+    @items.setter 
+    def items(self, a): 
+        self._items = a 
+
+    def addItem (self, a):
+        if (type(a) is shopping_item):
+            self._items.append(a)
+        else:
+            raise ValueError("Wrong item type") 
+
+    def print_items (self, verbose = False):
+
+        print ("\n Shopping basket items")
+        if (verbose) :
+            for i in self._items: 
+                print("{0}: {1} (cat: {2}, imported: {3})".format(i.name, i.price, i.category, i.imported))
+        else:
+            for i in self._items:
+                if i.imported: 
+                    print("{0} imported {1} \t at {2}\t(cat: {3})".format(i.quantity, i.name, i.price, i.category))
+                else:
+                    print("{0} {1} \tat {2}\t(cat: {3})".format(i.quantity, i.name, i.price, i.category))
+
+
+# rappresent a shooping receipt
+class shopping_receipt():
+
+    __free_tax_categories = ['books','food','medicalproducts']
+
+    def __init__(self, sb): 
+
+        self._shopping_basket = sb   
+        # calculate taxes after add shooping basket
+        self.__calculate_taxes ()
+
+    @property
+    def shopping_basket(self): 
+        return self._shopping_basket
+
+    def __calculate_taxes(self):
+
+        for i in self._shopping_basket.items:
+
+            tax_rate = 0
+
+            if i.category not in self.__free_tax_categories:
+                tax_rate = 10
+
+            if i.imported:
+                tax_rate = tax_rate + 5
+
+            i.tax = roundup_taxes((i.price * i.quantity * tax_rate)/100)
+
+    def print_receipt_details (self,):
+
+        print ("\n Receipt details")
+        for i in self._shopping_basket.items:
+            if i.imported:
+                print("{0} imported {1}: \t{2} \t (cat: {3})".format(i.quantity, i.name, round(i.price + i.tax, 2), i.category))
+            else:
+                print("{0} {1}: \t{2} \t (cat: {3})".format(i.quantity, i.name, round(i.price + i.tax, 2), i.category))
+        
+        print ("Sales Taxes: {0}".format(self.get_taxes()))
+        print ("Total: {0}".format(self.get_total()))
+
+    # get total of the receipt
+    def get_total (self):
+
+        total = 0
+        for i in self._shopping_basket.items:
+            total = total + round((i.price*i.quantity + i.tax), 2)
+
+        return round(total, 2)
+
+    # get total of the taxes
+    def get_taxes (self):
+        
+        total_taxes = 0
+        for i in self._shopping_basket.items:
+            total_taxes = roundup_taxes(total_taxes + i.tax)
+
+        return total_taxes
+
+
---
+++ test_01/test_sales.py
@@ -0,0 +1,383 @@
+from sales import shopping_receipt, shopping_basket, shopping_item, roundup_taxes
+import unittest
+
+
+class sales_test(unittest.TestCase):
+
+    # test zero
+    def test_unittest(self):       
+        self.assertEqual(1, 1)
+
+    # test round function
+    def test_round_function (self):
+
+        self.assertEqual (roundup_taxes(16.68), 16.7)
+        self.assertEqual (roundup_taxes(16.5), 16.5)
+        self.assertEqual (roundup_taxes(16.63), 16.65)
+        self.assertEqual (roundup_taxes(16.71), 16.75)
+        self.assertEqual (roundup_taxes(16.70), 16.7)
+
+    # test total sales testcase basket 1
+    def test_sales_taxes_basket_1 (self):
+
+        item1 = shopping_item()
+        item1.name = "The call of the wild"
+        item1.price = 12.49
+        item1.category = "books"
+
+        item2 = shopping_item()
+        item2.name = "Nevermind"
+        item2.price = 14.99
+        item2.category = "music CD"
+
+        item3 = shopping_item()
+        item3.name = "chocolate bar"
+        item3.price = 0.85
+        item3.category = "food"
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+        sb.addItem (item3)
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+        sb.print_items ()
+
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 3)
+
+        sr.print_receipt_details ()
+        self.assertEqual(sr.get_taxes (), 1.5)
+
+    # test total cost testcase basket 1
+    def test_total_cost_basket_1 (self):
+
+        item1 = shopping_item()
+        item1.name = "The call of the wild"
+        item1.price = 12.49
+        item1.category = "books"
+
+        item2 = shopping_item()
+        item2.name = "Nevermind"
+        item2.price = 14.99
+        item2.category = "music CD"
+
+        item3 = shopping_item()
+        item3.name = "chocolate bar"
+        item3.price = 0.85
+        item3.category = "food"
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+        sb.addItem (item3)
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+        sb.print_items ()
+
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 3)
+
+        sr.print_receipt_details ()
+
+        self.assertEqual(sr.get_total (), 29.83)
+
+    # test add items to shopping basket
+    def test_add_items_to_shoppingbasket (self):
+
+        item1 = shopping_item()
+        item1.name = "The call of the wild"
+        item1.price = 12.491
+        item1.category = "books"
+
+        item2 = shopping_item()
+        item2.name = "Nevermind"
+        item2.price = 14.991
+        item2.category = "music CD"
+
+        b = shopping_basket ()
+        b.addItem (item1)
+        b.addItem (item2)
+
+        self.assertEqual(len(b.items), 2)
+
+    # test add shopping basket to receipe
+    def test_add_shoppingbasket_to_receipe (self):
+
+        item1 = shopping_item()
+        item1.name = "The call of the wild"
+        item1.price = 12.491
+        item1.category = "books"
+
+        item2 = shopping_item()
+        item2.name = "Nevermind"
+        item2.price = 14.991
+        item2.category = "music CD"
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+
+        # create new receipt
+        r = shopping_receipt(sb)
+
+        self.assertNotEqual (r.shopping_basket, None)
+        self.assertEqual (len(r.shopping_basket.items), 2)
+
+    # test sales taxes testcase basket 2 
+    def test_sales_taxes_basket_2 (self):
+
+        item1 = shopping_item()
+        item1.name = "box of chocolates"
+        item1.price = 10.00
+        item1.category = "food"
+        item1.imported = True
+
+        item2 = shopping_item()
+        item2.name = "bottle of perfume"
+        item2.price = 47.50
+        item2.category = "generic"
+        item2.imported = True
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+        sb.print_items ()
+
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 2)
+
+        sr.print_receipt_details ()
+        self.assertEqual(sr.get_taxes(), 7.65)
+
+    # test total cost testcase basket 2
+    def test_total_cost_basket_2 (self):
+
+        item1 = shopping_item()
+        item1.name = "box of chocolates"
+        item1.price = 10.00
+        item1.category = "food"
+        item1.imported = True
+
+        item2 = shopping_item()
+        item2.name = "bottle of perfume"
+        item2.price = 47.50
+        item2.category = "generic"
+        item2.imported = True
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+        sb.print_items ()
+
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 2)
+
+        sr.print_receipt_details ()
+        self.assertEqual(sr.get_total (), 65.15)
+
+    # test sale taxes testcase basket 3
+    def test_sale_taxes_basket_3 (self):
+
+        item1 = shopping_item()
+        item1.name = "bottle of perfume"
+        item1.price = 27.99
+        item1.category = "generic"
+        item1.imported = True
+
+        item2 = shopping_item()
+        item2.name = "bottle of perfume"
+        item2.price = 18.99
+        item2.category = "generic"
+        item2.imported = False
+
+        item3 = shopping_item()
+        item3.name = "packet of headache pills"
+        item3.price = 9.75
+        item3.category = "medicalproducts"
+        item3.imported = False
+
+        item4 = shopping_item()
+        item4.name = "box of chocolates"
+        item4.price = 11.25
+        item4.category = "food"
+        item4.imported = True
+
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+        sb.addItem (item3)
+        sb.addItem (item4)
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+        sb.print_items ()
+
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 4)
+
+        sr.print_receipt_details ()
+        self.assertEqual(sr.get_taxes(), 6.70)
+
+        # test sale taxes testcase basket 3
+    
+    # test total cost testcase basket 3
+    def test_total_cost_basket_3 (self):
+
+        item1 = shopping_item()
+        item1.name = "bottle of perfume"
+        item1.price = 27.99
+        item1.category = "generic"
+        item1.imported = True
+
+        item2 = shopping_item()
+        item2.name = "bottle of perfume"
+        item2.price = 18.99
+        item2.category = "generic"
+        item2.imported = False
+
+        item3 = shopping_item()
+        item3.name = "packet of headache pills"
+        item3.price = 9.75
+        item3.category = "medicalproducts"
+        item3.imported = False
+
+        item4 = shopping_item()
+        item4.name = "box of chocolates"
+        item4.price = 11.25
+        item4.category = "food"
+        item4.imported = True
+
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+        sb.addItem (item3)
+        sb.addItem (item4)
+
+        sb.print_items ()
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 4)
+
+        sr.print_receipt_details ()
+        self.assertEqual(sr.get_total(), 74.68)
+
+    # test sale taxes testcase basket 3
+    def test_sale_taxes_basket_4 (self):
+
+        item1 = shopping_item()
+        item1.name = "bottle of perfume"
+        item1.price = 27.99
+        item1.quantity = 2
+        item1.category = "generic"
+        item1.imported = True
+
+        item2 = shopping_item()
+        item2.name = "bottle of perfume"
+        item2.price = 18.99
+        item2.category = "generic"
+        item2.imported = False
+
+        item3 = shopping_item()
+        item3.name = "packet of headache pills"
+        item3.price = 9.75
+        item3.quantity = 2
+        item3.category = "medicalproducts"
+        item3.imported = False
+
+        item4 = shopping_item()
+        item4.name = "box of chocolates"
+        item4.price = 11.25
+        item4.category = "food"
+        item4.imported = True
+
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+        sb.addItem (item3)
+        sb.addItem (item4)
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+        sb.print_items ()
+
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 4)
+
+        sr.print_receipt_details ()
+        self.assertEqual(sr.get_taxes(), 10.9)
+
+        # test sale taxes testcase basket 3
+    
+    # test total cost testcase basket 3
+    def test_total_cost_basket_4 (self):
+
+        item1 = shopping_item()
+        item1.name = "bottle of perfume"
+        item1.price = 27.99
+        item1.quantity = 2
+        item1.category = "generic"
+        item1.imported = True
+
+        item2 = shopping_item()
+        item2.name = "bottle of perfume"
+        item2.price = 18.99
+        item2.category = "generic"
+        item2.imported = False
+
+        item3 = shopping_item()
+        item3.name = "packet of headache pills"
+        item3.price = 9.75
+        item3.quantity = 2
+        item3.category = "medicalproducts"
+        item3.imported = False
+
+        item4 = shopping_item()
+        item4.name = "box of chocolates"
+        item4.price = 11.25
+        item4.category = "food"
+        item4.imported = True
+
+
+        # create new shopping basket
+        sb = shopping_basket ()
+        sb.addItem (item1)
+        sb.addItem (item2)
+        sb.addItem (item3)
+        sb.addItem (item4)
+
+        sb.print_items ()
+
+        # create new receipt
+        sr = shopping_receipt(sb)
+        
+        self.assertNotEqual (sr.shopping_basket, None)
+        self.assertEqual(len(sr.shopping_basket.items), 4)
+
+        sr.print_receipt_details ()
+        self.assertEqual(sr.get_total(), 116.62)
+
+if __name__ == '__main__':
+    unittest.main()
